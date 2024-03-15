c8f08140cf8141097f847e1e0d41239cf5748562
ayres01 <139259824+ayres01@users.noreply.github.com>
2024-03-15 14:06:05 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,50 +1,7 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"]
-}
-
-resource "aws_instance" "testserver" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "HelloWorld2"
-  }
-}
-
-data "aws_ami" "amzn-linux-2023-ami" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["al2023-ami-2023.*-x86_64"]
-  }
-}
-
-resource "aws_instance" "example" {
-  ami           = data.aws_ami.amzn-linux-2023-ami.id
-  instance_type = "t2.micro"
-  tags = {
-    Name = "HelloWorld1"
-  }
-}
-
 module "ec2_instance" {
   source = "./modules/ec2"
 
-  name = "single-instance"
+  name = "single-instance1"
 
   instance_type               = "t2.micro"
   key_name                    = "jenKP"
